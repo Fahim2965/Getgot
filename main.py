8b8ad68b199a33a5bedd279b7c2241e4351126de
Fahim Muntasir <famuntasir2@brooklynsteamcenter.org>
2024-01-30 09:20:13 -0500

    ok

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_name = "goodjob"
+    
+    return render_template("home.html.jinja", user_name = user_name)
+
+@app.route('/ping')
+def bub():
+    return "<h1> Pong <h1>"
